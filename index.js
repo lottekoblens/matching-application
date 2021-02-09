843debfa3c8161d6f5326badaf7b5f650f92d4e8
lottekoblens <lotte.koblens@hva.nl>
2021-02-09 16:37:41 +0100

    installed express

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const camelCase = require('camelcase');
 camelCase('foo-bar');
 console.log(camelCase('foo-bar'));
 //camelcase werkt goed
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is the about');
+})
+
+app.get('/like', (req, res) => {
+    res.send('This is the like page');
+})
+
+app.listen(3000);
