2b3a9a733629fba71901c80b1f440a27fb06a589
lottekoblens <lotte.koblens@hva.nl>
2021-02-17 16:35:09 +0100

    added form to partials

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const exphbs = require('express-handlebars');
 const { url } = require('inspector');
 const PORT = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
+const slug = require('slug');
 
 // express
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -25,19 +27,20 @@ app.get('/', function (req, res) {
 app.get('/like', (req, res) => {
     res.render('like', {
          style: 'like.css',
-         information: [
-            {info:'24 jaar, Amsterdam'}, 
-            {info: '26 jaar, Amstelveen'},
-            {info: '23 jaar, Diemen'},
-            {info:'24 jaar, Hoofddorp'},
-            {info:'23 jaar, Amsterdam'}
-        ],
+        //  information: [
+        //     {info:'24 jaar, Amsterdam'}, 
+        //     {info: '26 jaar, Amstelveen'},
+        //     {info: '23 jaar, Diemen'},
+        //     {info:'24 jaar, Hoofddorp'},
+        //     {info:'23 jaar, Amsterdam'}
+        // ],
          people: [
-            {name:'Lisa Hofman'}, 
-            {name:'Thomas Bergen'},
-            {name:'Julia Fransen'},
-            {name:'Sanne Groot'},
-            {name:'Kim Verdonge'}
+            {name:'Lisa Hofman', age:'24 jaar', residence: 'Amsterdam'}, 
+            {name:'Thomas Bergen', age: '26 jaar', residence: 'Amstelveen'},
+            {name:'Julia Fransen', age:'23 jaar', residence: 'Diemen'},
+            {name:'Sanne Groot', age:'24 jaar', residence: 'Hoofddorp'},
+            {name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam'},
+            {name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam'}
         ],
          product: [
             {option:'Hamburger'}, 
