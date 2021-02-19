e5b87ba0523f60a6414545f75b5067b8dfce4b69
lottekoblens <lotte.koblens@hva.nl>
2021-02-19 16:51:13 +0100

    had to change from handlebars to ejs

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 const express = require('express');
 const app = express();
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 const { url } = require('inspector');
 const PORT = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 const slug = require('slug');
+const ejs = require('ejs');
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const people =  [
+    {id: 0, img: '/static/images/girl-mc.jpg', name:'Lisa Hofman', age:'24 jaar', residence: 'Amsterdam', product: 'Hamburger'}, 
+    {id: 1, img: '/static/images/man-cheeseburger.jpg',name:'Thomas Bergen', age: '26 jaar', residence: 'Amstelveen', product: 'Cheeseburger'},
+    {id: 2, img: '/static/images/girl-cheeseburger.jpg',name:'Julia Fransen', age:'23 jaar', residence: 'Diemen', product:'Cheeseburger'},
+    {id: 3, img: '/static/images/woman-burger.jpg',name:'Sanne Groot', age:'24 jaar', residence: 'Hoofddorp', product:'Hamburger'},
+    {id: 4, img: '/static/images/girl-bigmac.jpg',name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam', product: 'Big Mac'},
+    {id: 5, img: '/static/images/girl-mc.jpg',name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam', product: 'Hamburger'}
+];
+
+// console.log(people[1].name);
 
 // express
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 // handlebars
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+// app.engine('ejs', ejs({ defaultLayout: 'header' }));
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
 // routing
 app.get('/', function (req, res) {
-    res.render('home', { 
-        style: 'home.css',
-        info: '24 jaar, Amsterdam',
-        name: 'Lisa Hofman'});
+    res.render('home',  
+    {style: 'home.css', 
+    name: 'lisa'});
 })
+
+
 
 app.get('/like', (req, res) => {
     res.render('like', {
          style: 'like.css',
-        //  information: [
-        //     {info:'24 jaar, Amsterdam'}, 
-        //     {info: '26 jaar, Amstelveen'},
-        //     {info: '23 jaar, Diemen'},
-        //     {info:'24 jaar, Hoofddorp'},
-        //     {info:'23 jaar, Amsterdam'}
-        // ],
          people: [
-            {name:'Lisa Hofman', age:'24 jaar', residence: 'Amsterdam', product: 'Hamburger'}, 
-            {name:'Thomas Bergen', age: '26 jaar', residence: 'Amstelveen', product: 'Cheeseburger'},
-            {name:'Julia Fransen', age:'23 jaar', residence: 'Diemen', product:'Cheeseburger'},
-            {name:'Sanne Groot', age:'24 jaar', residence: 'Hoofddorp', product:'Hamburger'},
-            {name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam', product: 'Big Mac'},
-            {name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam', product: 'Hamburger'}
-        ],
-         product: [
-            {option:'Hamburger'}, 
-            {option: 'Cheeseburger'},
-            {option:'Big Mac'}
-        ] });
-
-        // function randomProfile(people){
-        //     return people[Math.floor(Math.random()*people.length)];
-        // }
-        // console.log(randomProfile(people));
+            {img: '/static/images/girl-mc.jpg', name:'Lisa Hofman', age:'24 jaar', residence: 'Amsterdam', product: 'Hamburger'}, 
+            {img: '/static/images/man-cheeseburger.jpg',name:'Thomas Bergen', age: '26 jaar', residence: 'Amstelveen', product: 'Cheeseburger'},
+            {img: '/static/images/girl-cheeseburger.jpg',name:'Julia Fransen', age:'23 jaar', residence: 'Diemen', product:'Cheeseburger'},
+            {img: '/static/images/woman-burger.jpg',name:'Sanne Groot', age:'24 jaar', residence: 'Hoofddorp', product:'Hamburger'},
+            {img: '/static/images/girl-bigmac.jpg',name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam', product: 'Big Mac'},
+            {img: '/static/images/girl-mc.jpg',name:'Kim Verdonge', age:'23 jaar', residence: 'Amsterdam', product: 'Hamburger'}
+        ]
+    });
 });
+
+// function randomProfile(people){
+//     return people[Math.floor(Math.random()*people.length)];
+// }
+// console.log(randomProfile(people));
 
 //body-parser
 // support parsing of application/json type post data
@@ -61,22 +65,13 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/like",function(req,res){
-    const data = req.body;
+app.post("/", urlencodedParser, function(req,res){
+    // console.log(req.body.people);
+    // const data = req.body;
     res.send("response");
-    res.redirect('/');
+    // res.redirect('home');
+    // res.render ('like', { data });
 })
-
-// app.post('/like', like);
-
-// function like(req, res) {
-//     data.push ({
-//         name: req.body.name,
-//         age: req.body.age,
-//         residence: req.body.residence
-//     })
-//     res.redirect('/')
-// }
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!");
