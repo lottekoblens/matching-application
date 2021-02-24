bbf2636f89a69baecf02d15636456bf34511c7c5
lottekoblens <lotte.koblens@hva.nl>
2021-02-24 16:39:27 +0100

    liked people get displaye at like page

--- index.js
+++ index.js
@@ -12,57 +12,6 @@ const mongoose = require('mongoose');
 const testingModel = require('./models/test.model');
 require('dotenv').config();
 
-// const people = [
-// {
-// id: 0,
-// img: '/static/images/girl-mc.jpg',
-// name: 'Lisa Hofman',
-// age: '24 jaar',
-// residence: 'Amsterdam',
-// product: 'Hamburger',
-// },
-// {
-// id: 1,
-// img: '/static/images/man-cheeseburger.jpg',
-// name: 'Thomas Bergen',
-// age: '26 jaar',
-// residence: 'Amstelveen',
-// product: 'Cheeseburger',
-// },
-// {
-// id: 2,
-// img: '/static/images/girl-cheeseburger.jpg',
-// name: 'Julia Fransen',
-// age: '23 jaar',
-// residence: 'Diemen',
-// product: 'Cheeseburger',
-// },
-// {
-// id: 3,
-// img: '/static/images/woman-burger.jpg',
-// name: 'Sanne Groot',
-// age: '24 jaar',
-// residence: 'Hoofddorp',
-// product: 'Hamburger',
-// },
-// {
-// id: 4,
-// img: '/static/images/girl-bigmac.jpg',
-// name: 'Kim Verdonge',
-// age: '23 jaar',
-// residence: 'Amsterdam',
-// product: 'Big Mac',
-// },
-// {
-// id: 5,
-// img: '/static/images/girl-mc.jpg',
-// name: 'Kim Verdonge',
-// age: '23 jaar',
-// residence: 'Amsterdam',
-// product: 'Hamburger',
-// },
-// ];
-
 const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
 mongoose.connect(dbUrl, {
   useUnifiedTopology: true,
@@ -100,20 +49,19 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
   updateData(req.body.id, req.body.liked);
   res.redirect('/');
 });
 
-app.get('/like', (req, res) => {
+app.get('/like', async (req, res) => {
+  const liked = await findAllPeopleLiked();
+  const likedPeople = liked[0].id;
   res.render('like', {
     style: 'like.css',
-    people,
+    liked,
+    likedPeople,
   });
-});
-
-app.get('/profile/:id', function (request, response) {
-  response.send(request.profileId);
 });
 
 app.use(function (req, res, next) {
@@ -127,7 +75,7 @@ function updateData(id, liked) {
     .findOneAndUpdate({ id }, { $set: { liked, visited: true } })
     .lean()
     .then((data) => {
-      console.log(data);
+      // console.log(data);
     });
 }
 
@@ -138,5 +86,13 @@ async function findAllPeopleNotVisited() {
     .then((data) => data);
   // console.log(data);
 
+  return data;
+}
+
+async function findAllPeopleLiked() {
+  const data = await testingModel
+    .find({ liked: true })
+    .lean()
+    .then((data) => data);
   return data;
 }
