efb1e3f4975440e8e1c9c6b27ac87890117573e4
lottekoblens <lotte.koblens@hva.nl>
2021-03-05 10:06:09 +0100

    working on a popup

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,12 @@
-// const button = document.getElementById('button1');
-// button.addEventListener('submit', clickedLike);
+const button = document.getElementById('button1');
+button.addEventListener('submit', clickedLike);
+const popup = document.getElementById('LikedPopup');
 
-// function clickedLike() {
-// }
+function clickedLike() {
+  window.setTimeout(hidePopup(), 2000);
+  popup.classList.add('show');
+}
+
+function hidePopup() {
+  popup.classList.remove('show');
+}
