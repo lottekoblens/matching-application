10d0d721f4dbc87ac3bb1297b5769177546b5fb1
lottekoblens <lotte.koblens@hva.nl>
2021-03-06 14:12:51 +0100

    added some comments

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const mongoose = require('mongoose'); // used mongoose for connection with database
 const testingModel = require('./models/test.model');
 require('dotenv').config();
+// source dotenv: https://www.npmjs.com/package/dotenv
 
 // database connection
 const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`;
@@ -19,6 +20,7 @@ mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
+// Source connecting with mongoose: https://mongoosejs.com/docs/guide.html
 
 // express
 app
@@ -42,6 +44,7 @@ app
       console.log(e);
       res.redirect('/nomorepeople');
     }
+    // sources for try catch method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch, https://www.w3schools.com/js/js_errors.asp, https://javascript.info/try-catch
   })
   .get('/like', async (req, res) => {
     try {
@@ -100,6 +103,7 @@ app
 function updateData(id, liked) {
   testingModel
     .findOneAndUpdate({ id }, { $set: { liked, visited: true } })
+    // source findOneAndUpdate: https://mongoosejs.com/docs/tutorials/findoneandupdate.html
     .lean()
     // lean zorgt dat querie sneller wordt uitgevoerd (is not necesarry)
     .then((data) => {
@@ -122,6 +126,7 @@ async function findAllPeopleNotVisited() {
   const data = await testingModel.find({ visited: false }).lean();
   return data;
 }
+// source async funtions: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
 
 // people of which liked is true will be found
 async function findAllPeopleLiked() {
